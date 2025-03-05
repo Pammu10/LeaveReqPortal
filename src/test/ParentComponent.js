81eeb10ca79f74cde280ad14a51789fae81962c2
Pramodh Krishna <pramodh380.com>
2025-03-05 12:07:55 +0530

    final react

--- src/test/ParentComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ChildComponent from './ChildComponent'
-
-const ParentComponent = () => {
-  return <ChildComponent name="Cas"/>
-}
-
-export default ParentComponent
